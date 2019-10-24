7ddd0a835dab9fba77a938a5caeb1085f39495a0
Arman Ashrafian <ashy63@gmail.com>
2019-10-24 12:47:13 -0700

    setting up the repo

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCasQxBoLwDZSKRqYT4GL7QCkIHTi8xtM",
+    authDomain: "libwalk-721c2.firebaseapp.com",
+    databaseURL: "https://libwalk-721c2.firebaseio.com",
+    projectId: "libwalk-721c2",
+    storageBucket: "libwalk-721c2.appspot.com",
+    messagingSenderId: "643234122966",
+    appId: "1:643234122966:web:c8fde76f57972e65af8d77",
+    measurementId: "G-QB8877TYNB"
+};
