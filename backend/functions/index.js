ca9f5d31829ce0f5fce401b2cc3d72918db6894a
Bao <bghoang.ucsd.edu>
2019-11-10 14:08:15 -0800

    add getClubs

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,11 +1,11 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
 // test function
-exports.helloWorld = functions.https.onRequest((req, res) => {
- res.send("HIIIII");
+/*exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("HIIIII");
 });
 
 // test function
@@ -13,20 +13,38 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
 exports.testDb = functions.https.onRequest((req, res) => {
   admin
     .firestore()
-    .collection('Test').doc('Arman')
+    .collection("Test")
+    .doc("Arman")
     .get()
-    .then( (doc) => { // db query success
+    .then(doc => {
+      // db query success
       let age = doc.data().Age; // doc.data() returns a JSON object
-      res.send({age: age}); // res.send() parameter must be JSON !!
+      res.send({ age: age }); // res.send() parameter must be JSON !!
     })
-    .catch( (err) => { // db query fail
+    .catch(err => {
+      // db query fail
       res.send(err);
     });
 });
 
 exports.getUser = functions.https.onRequest((req, res) => {
-  res.send('get user');
+  res.send("get user");
 });
-
-
-
+*/
+exports.getClubs = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("Clubs")
+    .get()
+    .then(querySnapshot => {
+      let json_data = { clubs: [] };
+      querySnapshot.forEach(doc => {
+        json_data.clubs.push(doc.data());
+      });
+      res.send(json_data);
+    })
+    .catch(err => {
+      // db query fail
+      res.send(err);
+    });
+});
