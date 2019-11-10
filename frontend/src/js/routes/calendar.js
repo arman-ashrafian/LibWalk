1388da8fd35545cd9621e57ebf0b08da20a27501
kaustubh <klall@ucsd.edu>
2019-11-10 11:15:29 -0800

    Calendar should now extend react component

--- frontend/src/js/routes/calendar.js
+++ frontend/src/js/routes/calendar.js
@@ -5,14 +5,15 @@ class Calendar extends React.Component {
     constructor(props) {
         super(props) ;
         this.setState(this.props);
-        console.log('Calendar set its state to', this.state);
+        this.props = {...this.props, description : [0, 1, 2, 3, 4, 5, 6 , 7 , 8, 9]};
+        console.log('Calendar set its state to', this.props );
     }
 
     render() {
         return this.draw_calendar();
     }
 
-    draw_calendar(props) {
+    draw_calendar() {
         return (
             <div className='calendar_page'>
                 <h1> CALENDAR </h1>
@@ -38,16 +39,26 @@ class Calendar extends React.Component {
         </div>);
     }
 
-    calendar_week(props) {
+    calendar_week() {
+
         return (
             <div className="calendar__week">
-                <div className="calendar__day day">{this.props.description[0]}</div>
-                <div className="calendar__day day">{this.props.description[1]}</div>
-                <div className="calendar__day day">{this.props.description[2]}</div>
-                <div className="calendar__day day">{this.props.description[3]}</div>
-                <div className="calendar__day day">{this.props.description[4]}</div>
-                <div className="calendar__day day">{this.props.description[5]}</div>
-                <div className="calendar__day day">{this.props.description[6]}</div>
+                {/*<div className="calendar__day day">{this.props.description[0]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[1]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[2]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[3]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[4]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[5]}</div>*/}
+                {/*<div className="calendar__day day">{this.props.description[6]}</div>*/}
+
+
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
+                <div className="calendar__day day">{'Test'}</div>
             </div>
         );
     }
@@ -62,59 +73,5 @@ class Calendar extends React.Component {
 
 
 }
-// function draw_calendar(props) {
-//     props = {
-//         description : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//     };
-//
-//     return (
-//         <div className='calendar_page'>
-//             <h1> CALENDAR </h1>
-//             {calendar_header(props)}
-//             {calendar_title(props)}
-//             {calendar_week(props)}
-//             {calendar_week(props)}
-//             {calendar_week(props)}
-//             {calendar_week(props)}
-//         </div>
-//     );
-// }
-//
-// function calendar_header(props) {
-//     return (<div className="calendar__header">
-//         <div>Mon</div>
-//         <div>Tue</div>
-//         <div>Wed</div>
-//         <div>Thu</div>
-//         <div>Fri</div>
-//         <div>Sat</div>
-//         <div>Sun</div>
-//     </div>);
-// }
-//
-// function calendar_week(props) {
-//     return (
-//         <div className="calendar__week">
-//             <div className="calendar__day day">{props.description[0]}</div>
-//             <div className="calendar__day day">{props.description[1]}</div>
-//             <div className="calendar__day day">{props.description[2]}</div>
-//             <div className="calendar__day day">{props.description[3]}</div>
-//             <div className="calendar__day day">{props.description[4]}</div>
-//             <div className="calendar__day day">{props.description[5]}</div>
-//             <div className="calendar__day day">{props.description[6]}</div>
-//         </div>
-//     );
-// }
-//
-// function calendar_title(props) {
-//     return (
-//         <div className="wow fadeIn">
-//             <h4 className="h2 text-center mb-5">{props.name}</h4>
-//         </div>
-//     );
-// }
 
-
-
-// export default draw_calendar;
 export default Calendar;
