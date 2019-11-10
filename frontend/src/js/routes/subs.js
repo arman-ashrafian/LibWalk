020b7b7ac514fcce5d528808de732050f6f6a78c
kaustubh <klall@ucsd.edu>
2019-11-10 14:18:05 -0800

    NavBar links works, will always draw over the sign in on sign in page.

--- frontend/src/js/routes/subs.js
+++ frontend/src/js/routes/subs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../navbar";
+
 // import Sub from "../../components/sub";
 
 class Subs extends React.Component {
@@ -26,29 +27,30 @@ class Subs extends React.Component {
             }
         };
 
-        return (<div>
-            <NavBar {...this.props}/>
+        return (
+            <div>
+                <NavBar {...this.props}/>
 
-            <div className="col-lg-5 col-xl-4 mb-4">
-                {/*Featured image*/}
-                <div className="view overlay rounded z-depth-1">
-                    <img src={this.props.club_picture} className="img-fluid" alt=""/>
+                <div className="col-lg-5 col-xl-4 mb-4">
+                    {/*Featured image*/}
+                    <div className="view overlay rounded z-depth-1">
+                        <img src={this.props.club_picture} className="img-fluid" alt=""/>
+                    </div>
+
                 </div>
 
-            </div>
-
-            <div className="col-lg-7 col-xl-7 ml-xl-4 mb-4">
-                <h3 className="mb-3 font-weight-bold dark-grey-text">
-                    <strong>{this.props.club_name}</strong>
-                </h3>
-                <p className="grey-text">{this.props.club_description}</p>
-                {this.props.club_tags.map(tag => (
-                    <a target="_blank" className="btn btn-danger btn-md">
-                        {tag}
-                    </a>
-                ))}
-            </div>
-        </div>);
+                <div className="col-lg-7 col-xl-7 ml-xl-4 mb-4">
+                    <h3 className="mb-3 font-weight-bold dark-grey-text">
+                        <strong>{this.props.club_name}</strong>
+                    </h3>
+                    <p className="grey-text">{this.props.club_description}</p>
+                    {this.props.club_tags.map(tag => (
+                        <a target="_blank" className="btn btn-danger btn-md">
+                            {tag}
+                        </a>
+                    ))}
+                </div>
+            </div>);
     }
 }
 
