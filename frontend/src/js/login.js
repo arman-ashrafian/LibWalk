caf4e17eb0737436e65886778746b5d73061492c
kaustubh <klall@ucsd.edu>
2019-11-09 19:07:09 -0800

    Frontend signup fixed

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -1,65 +1,59 @@
-function login() {
+import React from 'react';
+
+
+function login_screen() {
     return (
-        <div class="card">
+        <div className="card">
             <div>
-                <h5 class="card-header info-color white-text text-center py-4">
+                <h5 className="card-header info-color white-text text-center py-4">
                     <strong>Sign in</strong>
                 </h5>
             </div>
-
-            <div class="card-body px-lg-5 pt-0">
-
-                <form class="text-center" style="color: #757575;" action="#!">
-
-                    <div class="md-form">
-                        <input type="email" id="materialLoginFormEmail" class="form-control"/>
-                        <label for="materialLoginFormEmail">E-mail</label>
+            <div className="card-body px-lg-5 pt-0">
+                <form className="text-center" style={{color: '#757575'}} action="#!">
+                    <div className="md-form">
+                        <input type="email" id="materialLoginFormEmail" className="form-control"/>
+                        <label htmlFor="materialLoginFormEmail">E-mail</label>
                     </div>
-
-                    <div class="md-form">
-                        <input type="password" id="materialLoginFormPassword" class="form-control"/>
-                        <label for="materialLoginFormPassword">Password</label>
+                    <div className="md-form">
+                        <input type="password" id="materialLoginFormPassword" className="form-control"/>
+                        <label htmlFor="materialLoginFormPassword">Password</label>
                     </div>
-
-                    <div class="d-flex justify-content-around">
+                    <div className="d-flex justify-content-around">
                         <div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="materialLoginFormRemember"/>
-                                <label class="form-check-label" for="materialLoginFormRemember">Remember me</label>
+                            <div className="form-check">
+                                <input type="checkbox" className="form-check-input" id="materialLoginFormRemember"/>
+                                <label className="form-check-label" htmlFor="materialLoginFormRemember">Remember
+                                    me</label>
                             </div>
                         </div>
                     </div>
                     <div>
-                            <a href="">Forgot password?</a>
+                        <a href>Forgot password?</a>
                     </div>
-
-                    <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
+                    <button className="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
                             type="submit">Sign in
                     </button>
-
                     <p>Not a member?
-                        <a href="">Register</a>
+                        <a href>Register</a>
                     </p>
-
                     <p>or sign in with:</p>
-                    <a type="button" class="btn-floating btn-fb btn-sm">
-                        <i class="fab fa-facebook-f"></i>
+                    <a type="button" className="btn-floating btn-fb btn-sm">
+                        <i className="fab fa-facebook-f"/>
                     </a>
-                    <a type="button" class="btn-floating btn-tw btn-sm">
-                        <i class="fab fa-twitter"></i>
+                    <a type="button" className="btn-floating btn-tw btn-sm">
+                        <i className="fab fa-twitter"/>
                     </a>
-                    <a type="button" class="btn-floating btn-li btn-sm">
-                        <i class="fab fa-linkedin-in"></i>
+                    <a type="button" className="btn-floating btn-li btn-sm">
+                        <i className="fab fa-linkedin-in"/>
                     </a>
-                    <a type="button" class="btn-floating btn-git btn-sm">
-                        <i class="fab fa-github"></i>
+                    <a type="button" className="btn-floating btn-git btn-sm">
+                        <i className="fab fa-github"/>
                     </a>
-
                 </form>
-
             </div>
-
         </div>
     );
+};
 
-}
+export default login_screen;
