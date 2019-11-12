af28a0c91accbeafcc2329b7dcd344e444493451
kaustubh <klall@ucsd.edu>
2019-11-12 12:16:26 -0800

    Added missing CSS files.

--- frontend/src/js/routes/home.js
+++ frontend/src/js/routes/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../navbar";
+import Carousel from 'react-bootstrap/Carousel'
 
 class Home extends React.Component {
     constructor(props) {
@@ -10,8 +11,23 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <NavBar {...this.props}/>
-                <h1> HOME PAGE </h1></div>
+                <NavBar/>
+
+                {/*actual page*/}
+                <main className='mt-5 pt-5'>
+                    <NavBar {...this.props}/>
+                    <h1> Welcome</h1>
+                    {this.org_carousel()}
+                </main>
+            </div>
+        );
+    }
+
+    org_carousel = () => {
+        return (
+            <Carousel>
+
+            </Carousel>
         );
     }
 
