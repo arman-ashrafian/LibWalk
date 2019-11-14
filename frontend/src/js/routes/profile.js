ba70f38401b03d1679dc5da5ed1dadbc11c19de9
Chau Vu <chv004@ucsd.edu>
2019-11-14 11:40:01 -0800

    styled profile page

--- frontend/src/js/routes/profile.js
+++ frontend/src/js/routes/profile.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../css/bootstrap.min.css'
 import '../../css/mdb.lite.min.css'
 import '../../css/style.min.css'
 import NavBar from "../navbar";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import {FaUser, FaGraduationCap, FaSchool} from 'react-icons/fa';
+import {MdEmail} from 'react-icons/md';
+
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-        console.log('Profile constructor call.')
+        this.initialState = {
+            editMode: false
+        }
+
+        this.state = this.initialState
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
     }
 
+    close() {
+        this.setState(() => this.initialState)
+    }
+
+    open() {
+        this.setState({ editMode: true});
+    }
+
+    handleEdit() {
+        alert("submitted")
+    }
 
     render() {
         let props = {
-            userName: 'kaus',
-            userEmail: 'you cant have my email.',
+            userName: 'Gary G.',
+            userEmail: 'ggez@ucsd.edu',
             userMajor: 'CS',
-            userYear: 'Senior',
-            userUrl: "https://images-na.ssl-images-amazon.com/images/I/61MCdgpt%2BmL._SX425_.jpg",
+            userYear: 'Super Senior',
+            userUrl: "https://www.jacobsschool.ucsd.edu/faculty/faculty_bios/photos/300.jpg",
         };
 
         this.props = {...this.props, ...props};
@@ -25,47 +49,75 @@ class Profile extends React.Component {
         return (
             <main className='mt-5 pt-5'>
                 <NavBar {...this.props}/>
-                <div className='container'>
-                    <div className="row">
-                        {/*Display an image*/}
-                        <div className="col-sm-4 text-center">
-                            <img src={this.props.userUrl}
-                                 onError="src='http://ucsdguardian.org/wp-content/uploads/2015/04/sun-god-with-glasses.png'"
-                                 height={300} width={300}
-                                 alt={''}/>
-                        </div>
+                <div className='container centerPage' >
+                    <div className="row centerPage">
 
                         {/*Display User Information*/}
-                        <div className="col-sm-4 text-left">
-                            <h1 style={{textDecoration: 'underline'}}>
-                                My Information
-                            </h1>
+                        <div className="col-sm-12 text-center">
+                            <Image src={this.props.userUrl} height="150" width="150" roundedCircle />
+                        </div>
+                        <div className="div-centered ">
                             <h3>
-                                {this.props.userName}
+                                <FaUser /> Name : {this.props.userName}
                             </h3>
                             <h3>
-                                {this.props.userEmail}
+                                <MdEmail /> Email : {this.props.userEmail}
                             </h3>
                             <h3>
-                                {this.props.userMajor}
+                                <FaGraduationCap /> Major : {this.props.userMajor}
                             </h3>
                             <h3>
-                                {this.props.userYear}
+                                <FaSchool /> Year : {this.props.userYear}
                             </h3>
 
                         </div>
-                        <div className="col-sm-4">
-
-                            <h1 style={{textDecoration: 'underline'}}>
-                                Settings
-                            </h1>
-                            <button onClick={this.switch_view_login}>
-                                Logout
-                            </button>
-
+                        <div className="col-sm-12">
+                        <Button variant="outline-primary" size="lg" onClick={this.open}> 
+                            Edit 
+                        </Button>
+                        </div>
+                        <div className="col-sm-12">
+                        <Button variant="primary" size="m" onClick={this.switch_view_login}> 
+                            Log Out
+                        </Button>
                         </div>
                     </div>
                 </div>
+
+                <Modal show={this.state.editMode} onHide={this.close} size="m">
+                    <Modal.Header centered closeButton>
+                        <Modal.Title > Edit Profile </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                        <Form onSubmit={this.handleEdit}>
+                            <Form.Group controlId="formName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" placeholder="Enter Name" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter Email" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formMajor">
+                                <Form.Label>Major</Form.Label>
+                                <Form.Control type="major" placeholder="Enter Major" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formYear">
+                                <Form.Label>Year</Form.Label>
+                                <Form.Control type="year" placeholder="Enter Year" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" >
+                                Submit
+                            </Button>
+                        </Form>
+
+                        </div>
+                    </Modal.Body>
+                </Modal>
             </main>);
     }
 
