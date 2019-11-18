b8b6d3ea1dcd70a5f2254daf9246c7bbb69bfc95
Bao <bghoang.ucsd.edu>
2019-11-17 18:08:10 -0800

    tryna figure this pagination

--- frontend/src/components/clubInfo.jsx
+++ frontend/src/components/clubInfo.jsx
@@ -20,5 +20,21 @@ export default [
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
     clubTags: ["Academic", "Social", "surface", "tech"],
     clubDescription: "This is Undergraduate Student Council"
+  },
+  {
+    clubName: "Alpha Beta",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubdDescription: "This is Undergraduate Student Council",
+    clubTags: ["Social", "frat"],
+    clubDescription: "It's just a frat"
+  },
+  {
+    clubName: "Alpha Beta",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubdDescription: "This is Undergraduate Student Council",
+    clubTags: ["Social", "frat"],
+    clubDescription: "It's just a frat"
   }
 ];
