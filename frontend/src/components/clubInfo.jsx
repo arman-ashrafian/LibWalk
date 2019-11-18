12c48c2445a7184b1ed7896f5b78f74ec8b8127e
Bao <bghoang.ucsd.edu>
2019-11-18 13:25:35 -0800

    modify some data

--- frontend/src/components/clubInfo.jsx
+++ frontend/src/components/clubInfo.jsx
@@ -1,6 +1,13 @@
 export default [
   {
-    clubName: "ECE USC",
+    clubName: "BDE",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubTags: ["Academic", "No Life", "Weeb", "tech"],
+    clubDescription: "The Biggest Dev Energy in the class"
+  },
+  {
+    clubName: "USC",
     clubPicture:
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
     clubTags: ["Academic", "Social", "random tag", "surface", "tech"],
@@ -10,7 +17,6 @@ export default [
     clubName: "Alpha Beta",
     clubPicture:
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
-    clubdDescription: "This is Undergraduate Student Council",
     clubTags: ["Social", "frat"],
     clubDescription: "It's just a frat"
   },
@@ -18,23 +24,42 @@ export default [
     clubName: "IDK",
     clubPicture:
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
-    clubTags: ["Academic", "Social", "surface", "tech"],
-    clubDescription: "This is Undergraduate Student Council"
+    clubTags: ["Social", "frat"],
+    clubDescription: "Whatever"
   },
   {
-    clubName: "Alpha Beta",
+    clubName: "IDK",
     clubPicture:
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
-    clubdDescription: "This is Undergraduate Student Council",
     clubTags: ["Social", "frat"],
-    clubDescription: "It's just a frat"
+    clubDescription: "Whatever"
   },
   {
-    clubName: "Alpha Beta",
+    clubName: "IDK",
     clubPicture:
       "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
-    clubdDescription: "This is Undergraduate Student Council",
     clubTags: ["Social", "frat"],
-    clubDescription: "It's just a frat"
+    clubDescription: "Whatever"
+  },
+  {
+    clubName: "IDK",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubTags: ["Social", "frat"],
+    clubDescription: "Whatever"
+  },
+  {
+    clubName: "IDK",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubTags: ["Social", "frat"],
+    clubDescription: "Whatever"
+  },
+  {
+    clubName: "IDK",
+    clubPicture:
+      "https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500",
+    clubTags: ["Social", "frat"],
+    clubDescription: "Whatever"
   }
 ];
