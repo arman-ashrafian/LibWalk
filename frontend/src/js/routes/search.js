45dd07f69062dd21c1cb4e8e7146d8145058c817
kaustubh <klall@ucsd.edu>
2019-11-21 17:31:13 -0800

    Added admin home page, works *for now*

--- frontend/src/js/routes/search.js
+++ frontend/src/js/routes/search.js
@@ -6,21 +6,21 @@ class Search extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+
     render() {
         return (
-            <main className='mt-5 pt-5'>
-                <NavBar {...this.props}/>
-                <MdSearch />
-                <input type="text" className="input"
-                    placeholder="Search..." 
-                    style={{ margin: '0 auto', width: 500, fontSize:25, paddingLeft:20}}
-                />
-            </main>
+            <div>
+                <NavBar/>
+                <main className='mt-5 pt-5'>
+                    <MdSearch/>
+                    <input type="text" className="input"
+                           placeholder="Search..."
+                           style={{margin: '0 auto', width: 500, fontSize: 25, paddingLeft: 20}}
+                    />
+                </main>
+            </div>
         );
     }
-
-
 }
 
 
