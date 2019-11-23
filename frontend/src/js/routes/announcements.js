5dd86b13512e01acc9efdf03ba54840783d5ed75
Arman Ashrafian <ashy63@gmail.com>
2019-11-23 15:25:05 -0800

    fixed bug

--- frontend/src/js/routes/announcements.js
+++ frontend/src/js/routes/announcements.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../cloud.js'
-import '../../css/notifs.css'
+import React from "react";
+import "../cloud.js";
+import "../../css/notifs.css";
 import NavBar from "../navbar";
 import db from "../../firebase";
-import TimeAgo from '@jshimko/react-time-ago';
-import {getUser, getClubs, getAnnouncements} from "../cloud";
+import TimeAgo from "@jshimko/react-time-ago";
+import { getUser, getClubs, getAnnouncements } from "../cloud";
 import Toast from "react-bootstrap/Toast";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -15,52 +15,32 @@ import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Pagination from "react-bootstrap/Pagination";
 
-
 class Announcements extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            userId: "",
-            orgs: []
-        };
-
-        // GET /getClubs & set the state when the api response is recieved
-        getUser(this.state.userId).then((json) => {
-            this.setState({orgs: json.subscriptions});
-        });
-
-        if(this.state.orgs === undefined) {
-            this.state = {
-                orgs: []
-            };
-        }
+    this.state = {
+      userId: "",
+      orgs: []
     };
-
-    componentDidMount() {
-        db.auth().onAuthStateChanged(firebaseUser => {
-            if( firebaseUser) {
-                this.setState({ userId: firebaseUser.uid });
-            } else {
-                console.log("Redirecting to login page")
-            }
-
-        });
-    }
-
-    render() {
-        if(this.state.orgs === undefined) {
-            this.state = {
-                orgs: []
-            }
-        }
-
-    if (this.state.orgs === undefined) {
-      this.state = {
-        orgs: []
-      };
-    }
   }
+
+  componentDidMount() {
+    db.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        this.setState({ userId: firebaseUser.uid });
+        getUser(firebaseUser.uid).then(json => {
+          if ( !('subscriptions' in json) || (json.subscriptions.length === 0) ) {
+            alert("YOU HAVE NO SUBSCRIPTIONS DUMMY")
+          }
+          this.setState({orgs: json.subscriptions});
+        })
+      } else {
+        console.log("Redirecting to login page");
+      }
+    });
+  }
+
 
   render() {
     if (this.state.orgs === undefined) {
@@ -128,51 +108,57 @@ class Announcements extends React.Component {
     );
   };
 }
-    let club_grid = (org) => {
-        return (
-            <Card style={{width: '80rem', height: '20rem'}} className='text-center'>
-                <Card.Header>
-                    <strong style={{fontSize: 24}}> {org.clubName}</strong>
-                    <br />
-                    <small style={{fontSize: 16}}>{org.clubDescription}</small>
-                </Card.Header>
-                <Card.Body>
-                    <div className="div-centered">
-                        <MakeCard />
-                    </div>
-                </Card.Body>
-            </Card>
-        )
-    };
+let club_grid = org => {
+  return (
+    <Card style={{ width: "80rem", height: "20rem" }} className="text-center">
+      <Card.Header>
+        <strong style={{ fontSize: 24 }}> {org.clubName}</strong>
+        <br />
+        <small style={{ fontSize: 16 }}>{org.clubDescription}</small>
+      </Card.Header>
+      <Card.Body>
+        <div className="div-centered">
+          <MakeCard />
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
-    function MakeCard() {
-        return (
-            <Row>
-                <Card border="info" style={{fontSize: 12}}>
-                    <Card.Header>
-                        <strong className="mr-auto">Notification</strong>
-                    </Card.Header>
-                    <Card.Body>Pizza Night</Card.Body>
-                    <Card.Footer>Last posted <TimeAgo date='Nov 19, 2019' /></Card.Footer>
-                </Card>
-                <br />
-                <Card border="warning" style={{fontSize: 12}}>
-                    <Card.Header>
-                        <strong className="mr-auto">Notification</strong>
-                    </Card.Header>
-                    <Card.Body> Free Boba Tomorrow Night</Card.Body>
-                    <Card.Footer>Last posted <TimeAgo date='Nov 19, 2019' /></Card.Footer>
-                </Card>
-                <br />
-                <Card border="danger" style={{fontSize: 12}}>
-                    <Card.Header>
-                        <strong className="mr-auto">Notification</strong>
-                    </Card.Header>
-                    <Card.Body>First Meeting Starts @ 12pm on 11/11/2019</Card.Body>
-                    <Card.Footer>Last posted <TimeAgo date='Nov 19, 2019' /></Card.Footer>
-                </Card>
-            </Row>
-        );
-    }
+function MakeCard() {
+  return (
+    <Row>
+      <Card border="info" style={{ fontSize: 12 }}>
+        <Card.Header>
+          <strong className="mr-auto">Notification</strong>
+        </Card.Header>
+        <Card.Body>Pizza Night</Card.Body>
+        <Card.Footer>
+          Last posted <TimeAgo date="Nov 19, 2019" />
+        </Card.Footer>
+      </Card>
+      <br />
+      <Card border="warning" style={{ fontSize: 12 }}>
+        <Card.Header>
+          <strong className="mr-auto">Notification</strong>
+        </Card.Header>
+        <Card.Body> Free Boba Tomorrow Night</Card.Body>
+        <Card.Footer>
+          Last posted <TimeAgo date="Nov 19, 2019" />
+        </Card.Footer>
+      </Card>
+      <br />
+      <Card border="danger" style={{ fontSize: 12 }}>
+        <Card.Header>
+          <strong className="mr-auto">Notification</strong>
+        </Card.Header>
+        <Card.Body>First Meeting Starts @ 12pm on 11/11/2019</Card.Body>
+        <Card.Footer>
+          Last posted <TimeAgo date="Nov 19, 2019" />
+        </Card.Footer>
+      </Card>
+    </Row>
+  );
+}
 
 export default Announcements;
