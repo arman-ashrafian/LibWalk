ed8a50b80e2ab86c97a505e521de14ef6d4d3da8
Chau Vu <chv004@ucsd.edu>
2019-12-07 12:34:10 -0800

    fixed editClub in admin_home

--- frontend/src/js/routes/admin_home.js
+++ frontend/src/js/routes/admin_home.js
@@ -140,15 +140,11 @@ class AdminHome extends React.Component {
         e.preventDefault();
         await this.setState({
             org: {
+                ...this.state.org,
                 clubName: e.target[0].value,
                 contactEmail: e.target[1].value,
                 pictureURL: e.target[2].value,
                 description: e.target[3].value,
-                clubReference: this.state.org.clubReference,
-                tags: this.state.org.tags,
-                announcements: this.state.org.announcements,
-                pageURL: this.state.org.pageURL,
-                eventList: this.state.org.eventList
             }
         });
         await changeClub(this.state.org.clubReference, this.state.org);
